900225957a385b84d275afc1e919edda73ea6f44
Pedro Hernández Villagómez <100489445+PedroHv58@users.noreply.github.com>
2023-09-16 17:54:22 -0600

    Tarea 2 - Variables y Tipos de Datos

--- Main.kt
+++ Main.kt
@@ -1,7 +1,13 @@
-fun main() {
-    print("HelloUPIICSA!")
-    print("Este es un mensaje en consola")
+//Variables y Tipos de Datos
 
-    println("HelloUPIICSA!")
-    println("Este es un mensaje en consola")
+const val PI = 3.1416f
+fun main() {
+    //NumeroDecimal = Radio
+    val NumeroDecimal = 18.5f
+    val Perimetro= Perimetro(NumeroDecimal)
+    println("El tipo de dato por defecto para un numero decimal es: ${NumeroDecimal::class.simpleName}")
+    println("El perimetro del circulo es : $Perimetro")
+}
+fun Perimetro(NumeroDecimal:Float): Float{
+    return PI*(2*NumeroDecimal)
 }
